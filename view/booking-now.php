828964144b7813087e78c329e82232942e41d92c
K4m5 <doquan15012002@gmail.com>
2024-06-16 23:32:43 +0700

    update booking

---
+++ view/booking-now.php
@@ -0,0 +1,174 @@
+
+
+                <!-- Begin content -->
+                <div id="page_content_wrapper" class="noheader">
+                    <div class="inner">
+                        <!-- Begin main content -->
+                        <div class="inner_wrapper">
+                            <div class="sidebar_content full_width">
+                                <div data-elementor-type="wp-post" data-elementor-id="741" class="elementor elementor-741" data-elementor-settings="[]">
+                                    <div class="elementor-inner">
+                                        <div class="elementor-section-wrap">
+                                            <section
+                                                class="elementor-section elementor-top-section elementor-element elementor-element-7f61d5b elementor-section-stretched elementor-section-height-full elementor-section-boxed elementor-section-height-default elementor-section-items-middle"
+                                                data-id="7f61d5b"
+                                                data-element_type="section"
+                                                data-settings='{"stretch_section":"section-stretched","background_background":"video","background_video_link":"https:\/\/www.youtube.com\/watch?v=rUYS9_wVIvQ","background_video_start":13,"background_video_end":77,"hoteller_ext_is_background_parallax":"false","hoteller_ext_is_background_on_scroll":"false"}'
+                                            >
+                                                <div class="elementor-background-video-container elementor-hidden-phone">
+                                                    <div class="elementor-background-video-embed"></div>
+                                                </div>
+                                                <div class="elementor-background-overlay"></div>
+                                                <div class="elementor-container elementor-column-gap-default">
+                                                    <div class="elementor-row">
+                                                        <div
+                                                            class="elementor-column elementor-col-100 elementor-top-column elementor-element elementor-element-581ff65"
+                                                            data-id="581ff65"
+                                                            data-element_type="column"
+                                                            data-settings='{"hoteller_ext_is_scrollme":"false","hoteller_ext_is_smoove":"false","hoteller_ext_is_parallax_mouse":"false","hoteller_ext_is_infinite":"false","hoteller_ext_is_fadeout_animation":"false"}'
+                                                        >
+                                                            <div class="elementor-column-wrap elementor-element-populated">
+                                                                <div class="elementor-widget-wrap">
+                                                                    <div
+                                                                        class="elementor-element elementor-element-e64e029 elementor-widget elementor-widget-heading"
+                                                                        data-id="e64e029"
+                                                                        data-element_type="widget"
+                                                                        data-settings='{"hoteller_ext_is_scrollme":"false","hoteller_ext_is_smoove":"false","hoteller_ext_is_parallax_mouse":"false","hoteller_ext_is_infinite":"false","hoteller_ext_is_fadeout_animation":"false"}'
+                                                                        data-widget_type="heading.default"
+                                                                    >
+                                                                        <div class="elementor-widget-container">
+                                                                            <h1 class="elementor-heading-title elementor-size-default">
+                                                                                Your new Boutique Hotel in Prague.<br />
+                                                                                Plan the journey of your dreams
+                                                                            </h1>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div
+                                                                        class="elementor-element elementor-element-89d5d7d elementor-widget elementor-widget-hoteller-availability-search"
+                                                                        data-id="89d5d7d"
+                                                                        data-element_type="widget"
+                                                                        data-settings='{"hoteller_ext_is_scrollme":"false","hoteller_ext_is_smoove":"false","hoteller_ext_is_parallax_mouse":"false","hoteller_ext_is_infinite":"false","hoteller_ext_is_fadeout_animation":"false"}'
+                                                                        data-widget_type="hoteller-availability-search.default"
+                                                                    >
+                                                                        <div class="elementor-widget-container">
+                                                                            <div class="availability_search_wrapper">
+                                                                                <div class="mphb_sc_search-wrapper">
+                                                                                    <form method="GET" class="mphb_sc_search-form" action="https://themes.themegoods.com/hoteller/sites/search-results/">
+                                                                                        <p class="mphb-required-fields-tip">
+                                                                                            <small> Required fields are followed by <abbr title="required">*</abbr> </small>
+                                                                                        </p>
+                                                                                        <p class="mphb_sc_search-check-in-date">
+                                                                                            <label for="mphb_check_in_date-mphb-search-form-6061a38e85c0f"> Check-in <abbr title="Formatted as dd/mm/yyyy">*</abbr> </label>
+                                                                                            <br />
+                                                                                            <input
+                                                                                                id="mphb_check_in_date-mphb-search-form-6061a38e85c0f"
+                                                                                                data-datepick-group="mphb-search-form-6061a38e85c0f"
+                                                                                                value=""
+                                                                                                placeholder="Check-in Date"
+                                                                                                required="required"
+                                                                                                type="date"
+                                                                                                name="mphb_check_in_date"
+                                                                                                class="mphb-datepick"
+                                                                                                autocomplete="off"
+                                                                                            />
+                                                                                        </p>
+
+                                                                                        <p class="mphb_sc_search-check-out-date">
+                                                                                            <label for="mphb_check_out_date-mphb-search-form-6061a38e85c0f"> Check-out <abbr title="Formatted as dd/mm/yyyy">*</abbr> </label>
+                                                                                            <br />
+                                                                                            <input
+                                                                                                id="mphb_check_out_date-mphb-search-form-6061a38e85c0f"
+                                                                                                data-datepick-group="mphb-search-form-6061a38e85c0f"
+                                                                                                value=""
+                                                                                                placeholder="Check-out Date"
+                                                                                                required="required"
+                                                                                                type="date"
+                                                                                                name="mphb_check_out_date"
+                                                                                                class="mphb-datepick"
+                                                                                                autocomplete="off"
+                                                                                            />
+                                                                                        </p>
+
+                                                                                        <p class="mphb_sc_search-adults">
+                                                                                            <label for="mphb_adults-mphb-search-form-6061a38e85c0f"> Adults </label>
+                                                                                            <br />
+                                                                                            <select id="mphb_adults-mphb-search-form-6061a38e85c0f" name="mphb_adults">
+                                                                                                <option value="1" selected="selected">1</option>
+                                                                                                <option value="2">2</option>
+                                                                                                <option value="3">3</option>
+                                                                                                <option value="4">4</option>
+                                                                                                <option value="5">5</option>
+                                                                                                <option value="6">6</option>
+                                                                                                <option value="7">7</option>
+                                                                                                <option value="8">8</option>
+                                                                                                <option value="9">9</option>
+                                                                                                <option value="10">10</option>
+                                                                                                <option value="11">11</option>
+                                                                                                <option value="12">12</option>
+                                                                                                <option value="13">13</option>
+                                                                                                <option value="14">14</option>
+                                                                                                <option value="15">15</option>
+                                                                                                <option value="16">16</option>
+                                                                                                <option value="17">17</option>
+                                                                                                <option value="18">18</option>
+                                                                                                <option value="19">19</option>
+                                                                                                <option value="20">20</option>
+                                                                                                <option value="21">21</option>
+                                                                                                <option value="22">22</option>
+                                                                                                <option value="23">23</option>
+                                                                                                <option value="24">24</option>
+                                                                                                <option value="25">25</option>
+                                                                                                <option value="26">26</option>
+                                                                                                <option value="27">27</option>
+                                                                                                <option value="28">28</option>
+                                                                                                <option value="29">29</option>
+                                                                                                <option value="30">30</option>
+                                                                                            </select>
+                                                                                        </p>
+
+                                                                                        <p class="mphb_sc_search-children">
+                                                                                            <label for="mphb_children-mphb-search-form-6061a38e85c0f"> Children </label>
+                                                                                            <br />
+                                                                                            <select id="mphb_children-mphb-search-form-6061a38e85c0f" name="mphb_children">
+                                                                                                <option value="0" selected="selected">0</option>
+                                                                                                <option value="1">1</option>
+                                                                                                <option value="2">2</option>
+                                                                                                <option value="3">3</option>
+                                                                                                <option value="4">4</option>
+                                                                                                <option value="5">5</option>
+                                                                                                <option value="6">6</option>
+                                                                                                <option value="7">7</option>
+                                                                                                <option value="8">8</option>
+                                                                                                <option value="9">9</option>
+                                                                                                <option value="10">10</option>
+                                                                                            </select>
+                                                                                        </p>
+
+                                                                                        <input id="mphb_check_in_date-mphb-search-form-6061a38e85c0f-hidden" value="" type="hidden" name="mphb_check_in_date" />
+                                                                                        <input id="mphb_check_out_date-mphb-search-form-6061a38e85c0f-hidden" value="" type="hidden" name="mphb_check_out_date" />
+                                                                                        <p class="mphb_sc_search-submit-button-wrapper">
+                                                                                            <input type="submit" class="button" value="Search" />
+                                                                                        </p>
+                                                                                    </form>
+                                                                                </div>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </section>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End main content -->
+                    </div>
+                    <br class="clear" />
+                </div>
+
+
+            </div>
